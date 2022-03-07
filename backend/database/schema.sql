b1af3a7d7471fd662777d4d73ac3251b4d7b2a3e
MohamadAbdulraheem <mohammad.o.abdulraheem@gmail.com>
2022-03-07 22:58:31 +0300

    database

---
+++ backend/database/schema.sql
@@ -0,0 +1,81 @@
+DROP DATABASE MERAKI_Academy_Project_6;
+CREATE DATABASE MERAKI_Academy_Project_6 ;
+USE  MERAKI_Academy_Project_6 ;
+
+CREATE TABLE roles (
+id INT AUTO_INCREMENT NOT NULL,
+role VARCHAR(255) NOT NULL,
+PRIMARY KEY (id)
+);
+
+CREATE TABLE users(
+id INT AUTO_INCREMENT NOT NULL,
+firstName VARCHAR(255),
+lastName VARCHAR(255),
+age INT(3),
+country VARCHAR(255),
+email VARCHAR(255) UNIQUE NOT NULL,
+password VARCHAR(255) NOT NULL,
+user_image MEDIUMTEXT,
+role_id INT,
+FOREIGN KEY (role_id) REFERENCES roles(id),
+is_deleted TINYINT DEFAULT 0,
+PRIMARY KEY (id),
+publish_date DATETIME DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE videos (
+
+id INT AUTO_INCREMENT NOT NULL,
+title varchar(255),
+description TEXT,
+user_id INT,
+video_link MEDIUMTEXT,
+video_views INT DEFAULT 0,
+Likes INT DEFAULT 0,
+Dislikes INT DEFAULT 0,
+image MEDIUMTEXT,
+FOREIGN KEY (user_id) REFERENCES users(id),
+publish_date DATETIME DEFAULT CURRENT_TIMESTAMP,
+category VARCHAR(20),
+is_deleted TINYINT DEFAULT 0,
+PRIMARY KEY (id)
+);
+
+CREATE TABLE watch_later(
+id INT AUTO_INCREMENT NOT NULL,
+video_id INT,
+user_id INT,
+FOREIGN KEY (user_id) REFERENCES users(id),
+FOREIGN KEY (video_id) REFERENCES videos(id),
+PRIMARY KEY (id)
+);
+
+CREATE TABLE Liked_videos (
+id INT AUTO_INCREMENT NOT NULL,
+video_id INT,
+user_id INT,
+FOREIGN KEY (user_id) REFERENCES users(id),
+FOREIGN KEY (video_id) REFERENCES videos(id),
+PRIMARY KEY (id)
+);
+
+CREATE TABLE comments (
+id INT AUTO_INCREMENT NOT NULL,
+comment TEXT,
+user_id INT,
+video_id INT,
+FOREIGN KEY (user_id) REFERENCES users(id),
+FOREIGN KEY (video_id) REFERENCES videos(id),
+publish_date DATETIME DEFAULT CURRENT_TIMESTAMP,
+PRIMARY KEY (id)
+);
+
+CREATE TABLE subscriptions (
+id INT AUTO_INCREMENT NOT NULL,
+chanel_id INT,
+user_id INT,
+FOREIGN KEY (chanel_id) REFERENCES users(id),
+FOREIGN KEY (user_id) REFERENCES users(id),
+PRIMARY KEY (id)
+)
