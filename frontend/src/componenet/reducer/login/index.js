6614dcff77561a454df8c1f78b6a59eeb2556eb1
NasserQasim <nasseralotaiby1@gmail.com>
2022-03-09 15:41:10 +0300

    login-reducer

--- frontend/src/componenet/reducer/login/index.js
+++ frontend/src/componenet/reducer/login/index.js
@@ -2,3 +2,21 @@ const initialState = {
     token: localStorage.getItem("token"),
     isLoggedIn: localStorage.getItem("token") ? true : false,
   };
+
+  //create reducer function for login
+const loginReducer = (state = initialState, { type, payload }) => {
+    switch (type) {
+      case "LOG_IN":
+        return {
+          token: payload,
+          isLoggedIn: true,
+        };
+      case "LOG_OUT":
+        return {
+          token: null,
+          isLoggedIn: false,
+        };
+      default:
+        return state;
+    }
+  };
