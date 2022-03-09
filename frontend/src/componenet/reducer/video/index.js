303d2194b7e2cee33fff156aaf34afa9107d004e
NasserQasim <nasseralotaiby1@gmail.com>
2022-03-09 16:03:17 +0300

    login-reducer

--- frontend/src/componenet/reducer/video/index.js
+++ frontend/src/componenet/reducer/video/index.js
@@ -1,3 +1,39 @@
 const initialState = {
     videos: [],
   };
+
+
+
+  const videoReducer = (state = initialState, { type, payload }) => {
+    switch (type) {
+      case "ADD_VIDEO":
+        return {
+         videos = [...state,payload]
+        };
+      case "UPDATE_VIDEO":
+        return {
+          token: null,
+          isLoggedIn: false,
+        };
+     
+
+        case "DELETE_VIDEO":
+            return {
+               videos: state.videos.filter(video=>{
+                   return video.id === payload.
+               })
+            };
+        
+            case "GET_VIDEO":
+                return {
+                     payload
+                };    
+           
+                case "SET_VIDEOs":
+                    return {
+                      videos: payload
+                    };    
+                    default:
+                        return state;
+    }
+  };
