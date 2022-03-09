469e3be5a151f21fdd000763908b3448771683d9
MohamadAbdulraheem <mohammad.o.abdulraheem@gmail.com>
2022-03-09 21:34:05 +0300

    video

--- backend/routes/commentRouter.js
+++ backend/routes/commentRouter.js
@@ -5,34 +5,33 @@ const authentication = require("../middleware/authentication");
 const commentRouter = express.Router();
 
 const {
-    createNewComment,
-    updateCommentById,
-    deleteCommentById,
-    deleteCommentByUserId,
-    getAllComments,
-  } = require("../controllers/CommentControllers");
-  
-  //create end point for create comment
-  
-  commentRouter.post("/:id", authentication, createNewComment);
-  
-  //create end point for update comment
-  
-  commentRouter.put("/:id", authentication, updateCommentById);
-  
-  //create end point for delete comment by id
-  
-  commentRouter.delete("/delete_1/:id", authentication, deleteCommentById);
-  
-  //create end point for delete comment by user_id
-  
-  commentRouter.get("/video/:id",getAllComments)
-  
-  
-  commentRouter.delete(
-    "/delete_2/:user_id",
-    authentication,
-    deleteCommentByUserId
-  );
-  
-  module.exports = { commentRouter };
+  createNewComment,
+  updateCommentById,
+  deleteCommentById,
+  deleteCommentByUserId,
+  getAllComments,
+} = require("../controllers/CommentControllers");
+
+//create end point for create comment
+
+commentRouter.post("/:id", authentication, createNewComment);
+
+//create end point for update comment
+
+commentRouter.put("/:id", authentication, updateCommentById);
+
+//create end point for delete comment by id
+
+commentRouter.delete("/delete_1/:id", authentication, deleteCommentById);
+
+//create end point for delete comment by user_id
+
+commentRouter.get("/video/:id", getAllComments);
+
+commentRouter.delete(
+  "/delete_2/:user_id",
+  authentication,
+  deleteCommentByUserId
+);
+
+module.exports = { commentRouter };
