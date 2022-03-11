0a8e91eedaec35ffd5fecff73e71382462115c2e
NasserQasim <nasseralotaiby1@gmail.com>
2022-03-11 21:15:08 +0300

    LikedVideos Component

--- frontend/src/componenet/LikedVideos/LikedVideos.js
+++ frontend/src/componenet/LikedVideos/LikedVideos.js
@@ -2,43 +2,38 @@ import React, { useState } from "react";
 import { Routes, Route, Link } from "react-router-dom";
 import axios from "axios";
 import jwt_decode from "jwt-decode";
-import { useNavigate ,useEffect} from "react-router-dom";
+import { useNavigate, useEffect } from "react-router-dom";
 import { toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import { useDispatch, useSelector } from "react-redux";
 import { login, logout } from "../reducer/login/index";
 
+const LikedVideos = () => {
+  const navigate = useNavigate();
 
+  const state = useSelector((state) => {
+    return {
+      isLoggedIn: state.loginReducer.isLoggedIn,
+      token: state.loginReducer.token,
+    };
+  });
 
-const LikedVideos = ()=>{
+  const [LikedVideos, setLikedVideos] = useState([]);
 
-    
-    const [LikedVideos,setLikedVideos] = useState([])
+  axios
+    .get("http://localhost:5000/like", {
+      headers: {
+        Authorization: `Basic ${state.token}`,
+      },
+    })
+    .then((response) => {
+      setLikedVideos(response.data.results);
+    })
+    .catch((error) => {
+      console.log(error);
+    });
 
-  axios.get('http://localhost:5000/like',{
-    headers: {
-      Authorization: `Basic ${state.token}`,
-    },
-  }).then((response)=>{
-    setLikedVideos(response.data.results)
-
-  }).catch((error)=>{
-
-    console.log(error);
-  })
-  
-  return(
-<>
-
-
-
-
-
-</>
-
-)
-
-}
-
+  return <></>;
+};
 
 export default LikedVideos;
