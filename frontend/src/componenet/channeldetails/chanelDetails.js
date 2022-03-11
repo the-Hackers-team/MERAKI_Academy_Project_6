a4997abf428ad9807166ab66958f98f95d5c162d
NasserQasim <nasseralotaiby1@gmail.com>
2022-03-11 22:23:31 +0300

    channelDetails Component

--- frontend/src/componenet/channeldetails/chanelDetails.js
+++ frontend/src/componenet/channeldetails/chanelDetails.js
@@ -7,3 +7,13 @@ import { toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import { useDispatch, useSelector } from "react-redux";
 import { login, logout } from "../reducer/login/index";
+
+const ChannelDetails = () => {
+
+
+
+    
+  return <></>;
+};
+
+export default ChannelDetails;
