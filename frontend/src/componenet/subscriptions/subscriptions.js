24bc3ea41228fa1a1b737d6a4b6b65f9248b64bf
NasserQasim <nasseralotaiby1@gmail.com>
2022-03-11 22:00:01 +0300

    subscription component

--- frontend/src/componenet/subscriptions/subscriptions.js
+++ frontend/src/componenet/subscriptions/subscriptions.js
@@ -9,18 +9,21 @@ import { useDispatch, useSelector } from "react-redux";
 import { login, logout } from "../reducer/login/index";
 
 
-
+toast.configure();
 const Subcriptions = ()=>{
 
+
+    const [allSubscriptVideos,setallSubscriptVideos] = useState([])
     const getAllvideossubscripes = ()=>{
         axios.get("https://localhost:5000/subscription/allVideos", {
             headers: {
               Authorization: `Basic ${state.token}`,
             },
           }).then(response => {
+            setallSubscriptVideos(response.data.results);
 
           }).catch(error => {
-              
+    
           })
     }
 
