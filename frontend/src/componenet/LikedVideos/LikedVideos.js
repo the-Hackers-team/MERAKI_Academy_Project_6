0e43196cc01502f2cde789b82639edff17fa1af5
NasserQasim <nasseralotaiby1@gmail.com>
2022-03-11 21:14:06 +0300

    LikedVideos Component

---
+++ frontend/src/componenet/LikedVideos/LikedVideos.js
@@ -0,0 +1,44 @@
+import React, { useState } from "react";
+import { Routes, Route, Link } from "react-router-dom";
+import axios from "axios";
+import jwt_decode from "jwt-decode";
+import { useNavigate ,useEffect} from "react-router-dom";
+import { toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import { useDispatch, useSelector } from "react-redux";
+import { login, logout } from "../reducer/login/index";
+
+
+
+const LikedVideos = ()=>{
+
+    
+    const [LikedVideos,setLikedVideos] = useState([])
+
+  axios.get('http://localhost:5000/like',{
+    headers: {
+      Authorization: `Basic ${state.token}`,
+    },
+  }).then((response)=>{
+    setLikedVideos(response.data.results)
+
+  }).catch((error)=>{
+
+    console.log(error);
+  })
+  
+  return(
+<>
+
+
+
+
+
+</>
+
+)
+
+}
+
+
+export default LikedVideos;
