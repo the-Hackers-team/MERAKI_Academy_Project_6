b3548bcb9fdbecb1bfebea5f03223da629691d4d
NasserQasim <nasseralotaiby1@gmail.com>
2022-03-14 14:37:16 +0300

    update Video component

--- frontend/src/componenet/updateVideo/updateVideo.js
+++ frontend/src/componenet/updateVideo/updateVideo.js
@@ -7,20 +7,40 @@ import { toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import { useDispatch, useSelector } from "react-redux";
 
-const updateAnVideoById = ()=>{
+const updateAnVideoById = () => {
 
-let {id} = useParams()
+    const [title,setTitle] = useState("")
+    const [description,setDescription] = useState("")
+    const [image,setimage] = useState("")
+    const [category,setcategory] = useState("")
+    const [video_link,setvideo_link] = useState("")
 
-const getVideoById = ()=>{
-    axios.get(`http://localhost:5000/video/search_1?id=${id}`,{
+
+  const state = useSelector((state) => {
+    return {
+      isLoggedIn: state.loginReducer.isLoggedIn,
+      token: state.loginReducer.token,
+    };
+  });
+
+  let { id } = useParams();
+
+  const getVideoById = () => {
+    axios
+      .get(`http://localhost:5000/video/search_1?id=${id}`, {
         headers: {
           Authorization: `Basic ${state.token}`,
         },
-      }).then((response)=>{
-
-      }).catch((error)=>{
-          
       })
-}
-
-}
+      .then((response) => {
+        setTitle(response.data.results[0].title)
+        setDescription(response.data.results[0].description)
+        setimage(response.data.results[0].image)
+        setcategory(response.data.results[0].category)
+        setvideo_link(response.data.results[0].video_link)
+      })
+      .catch((error) => {
+          
+      });
+  };
+};
