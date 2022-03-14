dc8266f26ce174bd4587a47e3a348da494d1cc12
NasserQasim <nasseralotaiby1@gmail.com>
2022-03-14 14:32:29 +0300

    update Video component

--- frontend/src/componenet/updateVideo/updateVideo.js
+++ frontend/src/componenet/updateVideo/updateVideo.js
@@ -7,3 +7,20 @@ import { toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import { useDispatch, useSelector } from "react-redux";
 
+const updateAnVideoById = ()=>{
+
+let {id} = useParams()
+
+const getVideoById = ()=>{
+    axios.get(`http://localhost:5000/video/search_1?id=${id}`,{
+        headers: {
+          Authorization: `Basic ${state.token}`,
+        },
+      }).then((response)=>{
+
+      }).catch((error)=>{
+          
+      })
+}
+
+}
