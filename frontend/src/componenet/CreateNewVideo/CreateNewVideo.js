ece4fe200bb5332af604b9fea19dd6e74f73be90
SamerNoufal <samer.noufal13@gmail.com>
2022-03-14 21:14:08 +0300

    newVideo

--- frontend/src/componenet/CreateNewVideo/CreateNewVideo.js
+++ frontend/src/componenet/CreateNewVideo/CreateNewVideo.js
@@ -8,3 +8,17 @@ import "react-toastify/dist/ReactToastify.css";
 import { useDispatch, useSelector } from "react-redux";
 
 toast.configure();
+
+const CreateNewVideo = () => {
+    const [title, setTitle] = useState("");
+    const [description, setdescription] = useState("");
+    const [image, setimage] = useState("");
+    const [category, setcategory] = useState("");
+    const [video_link, setvideo_link] = useState("");
+    const state = useSelector((state) => {
+      return {
+        isLoggedIn: state.loginReducer.isLoggedIn,
+        token: state.loginReducer.token,
+      };
+    });
+
