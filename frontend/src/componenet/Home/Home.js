0145bcb727e00f2189e9ac3e694ebcbf66f4257a
MohamadAbdulraheem <mohammad.o.abdulraheem@gmail.com>
2022-03-16 00:21:03 +0300

    home

--- frontend/src/componenet/Home/Home.js
+++ frontend/src/componenet/Home/Home.js
@@ -37,28 +37,26 @@ const Home = () => {
       <h1>Recommended</h1>
 
       <div className="videos__container">
-       {videos&&videos.map((video)=>{
-return(<div className="video">
-<div className="video__thumbnail">
-  <img
-    src={video.image}
-    alt=""
-  />
-</div>
-<div className="video__details">
-  <div className="author">
-    <img src={videos.user_image} alt="" />
-  </div>
-  <div className="title">
-    <h3>
-     {video.title}
-    </h3>
-    <Link to="">{`${video.firstName}  ${video.lastName}` }</Link>
-    <span>{video.video_views} • 3 Months Ago</span>
-  </div>
-</div>
-</div>)
-       }) }
+        {videos &&
+          videos.map((video) => {
+            return (
+              <div className="video">
+                <div className="video__thumbnail">
+                  <img src={video.image} alt="" />
+                </div>
+                <div className="video__details">
+                  <div className="author">
+                    <img src={videos.user_image} alt="" />
+                  </div>
+                  <div className="title">
+                    <h3>{video.title}</h3>
+                    <Link to="">{`${video.firstName}  ${video.lastName}`}</Link>
+                    <span>{video.video_views} • 3 Months Ago</span>
+                  </div>
+                </div>
+              </div>
+            );
+          })}
       </div>
     </div>
   );
