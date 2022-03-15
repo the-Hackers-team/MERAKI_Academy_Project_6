8d7925d4e57801f055927586c50afd19d0002702
SamerNoufal <samer.noufal13@gmail.com>
2022-03-15 20:08:48 +0300

    newVideo

--- frontend/src/componenet/CreateNewVideo/CreateNewVideo.js
+++ frontend/src/componenet/CreateNewVideo/CreateNewVideo.js
@@ -128,4 +128,12 @@ const CreateNewVideo = () => {
           }}
         />
       </div>
-        {/* <div class="input-group mb-3">
+           {/* <div class="input-group mb-3">
+  <div class="input-group-prepend">
+    <span class="input-group-text">$</span>
+  </div>
+  <input type="text" class="form-control" aria-label="Amount (to the nearest dollar)"/>
+  <div class="input-group-append">
+    <span class="input-group-text">.00</span>
+  </div>
+</div> */}
