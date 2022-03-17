dc89b6f0fab7d9988670fa3c44f2a957f35bd7f4
NasserQasim <nasseralotaiby1@gmail.com>
2022-03-17 14:44:06 +0300

    css

--- frontend/src/componenet/profile/profile.js
+++ frontend/src/componenet/profile/profile.js
@@ -6,13 +6,14 @@ import { useNavigate } from "react-router-dom";
 import { toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import { useDispatch, useSelector } from "react-redux";
-import "./profile.css"
+import "./profile.css";
 toast.configure();
 const Profile = () => {
   const [userProfile, setuserProfile] = useState([]);
-  
+
   const [profileVideos, setprofileVideos] = useState([]);
   const [isDeleted, setisDeleted] = useState(false);
+  const [myVideos, setmyVideos] = useState(true);
   const navigate = useNavigate();
 
   const state = useSelector((state) => {
@@ -81,7 +82,7 @@ const Profile = () => {
   useEffect(() => {
     getUserById();
   }, []);
-console.log(userProfile);
+  console.log(userProfile);
   useEffect(() => {
     getAllVideosByChannelId();
   }, [isDeleted]);
@@ -89,43 +90,99 @@ console.log(userProfile);
   const profileDetails =
     userProfile &&
     userProfile.map((profile) => {
-      return(
-      <div className="profile-main">
-        <div className="profile-div-container">
-          <div className="container-above-div">
-            <div className="image-name">
-              <img src={profile.user_image} />
-              <span className="home-videos-playList2">{profile.firstName}</span>
-              <span className="home-videos-playList2">{profile.lastName}</span>
+      return (
+        <div className="profile-main">
+          <div className="profile-div-container">
+            <div className="container-above-div">
+              <div className="image-name">
+                <img src={profile.user_image} />
+                <span className="home-videos-playList2">
+                  {profile.firstName}
+                </span>
+                <span className="home-videos-playList2">
+                  {profile.lastName}
+                </span>
+              </div>
+              <div className="videos-profile">
+                <button
+                  className="all-videos"
+                  onClick={() => {
+                    setmyVideos(false);
+                  }}
+                >
+                  {" "}
+                  My Videos
+                </button>
+                <button className="all-videos">Delete Profile</button>
+              </div>
             </div>
-            <div className="videos-profile">
-              <button className="all-videos"> My Videos</button>
-              <button className="all-videos">Delete Profile</button>
+            <div className="home-videos-playList">
+              <div className="home-videos-playList2">Home</div>
+              <div
+                className="home-videos-playList2"
+                onClick={() => {
+                  setmyVideos(false);
+                }}
+              >
+                Videos
+              </div>
+              <div className="home-videos-playList2">Channels</div>
+              <div className="home-videos-playList2">About</div>
             </div>
           </div>
-          <div className="home-videos-playList">
-            <div className="home-videos-playList2">Home</div>
-            <div className="home-videos-playList2">Videos</div>
-            <div className="home-videos-playList2">Channels</div>
-            <div className="home-videos-playList2">About</div>
-          </div>
+
+          {myVideos ? (
+            <div className="profile-upload-videos">
+              <div className="profile-upload-image">
+                <img
+                  src="https://www.gstatic.com/youtube/img/channels/empty_channel_illustration.svg"
+                  className="image-upload-video"
+                />
+              </div>
+              <div className="uplaod-video-paragraph">
+                <p className="upload-pargraph">Upload a video to get started</p>
+                <p className="upload-pargraph-2">
+                  Start sharing your story and connecting with viewers. Videos
+                  you upload will show up here.
+                </p>
+                <button
+                  className="uplaod-button"
+                  onClick={() => {
+                    navigate("/create");
+                  }}
+                >
+                  Create New Video
+                </button>
+              </div>
+            </div>
+          ) : (
+            <div className="videos">
+              <div className="videos__container">
+                {profileVideos &&
+                  profileVideos.map((video) => {
+                    return (
+                      <div className="video">
+                        <div className="video__thumbnail">
+                          <img src={video.image} alt="" />
+                        </div>
+                        <div className="video__details">
+                          <div className="author">
+                            <img src={video.user_image} alt="" />
+                          </div>
+                          <div className="title">
+                            <h3>{video.title}</h3>
+                            <Link to="">{`${video.firstName}  ${video.lastName}`}</Link>
+                            <span>{video.video_views} • 3 Months Ago</span>
+                          </div>
+                        </div>
+                      </div>
+                    );
+                  })}
+              </div>
+            </div>
+          )}
         </div>
-       
-        <div className="profile-upload-videos">
-          <div className="profile-upload-image">
-            <img src="https://www.gstatic.com/youtube/img/channels/empty_channel_illustration.svg" className="image-upload-video" />
-          </div>
-          <div className="uplaod-video-paragraph">
-            <p className="upload-pargraph">Upload a video to get started</p>
-            <p className="upload-pargraph-2">
-              Start sharing your story and connecting with viewers. Videos you
-              upload will show up here.
-            </p>
-            <button className="uplaod-button">Create New Video</button>
-          </div>
-        </div>
-      </div>
-      )
+      );
     });
 
   return <>{profileDetails ? profileDetails : null}</>;
