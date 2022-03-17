a43ead07fdb8e6239c439f74dbb961bd6595f29e
NasserQasim <nasseralotaiby1@gmail.com>
2022-03-17 19:55:33 +0300

    modifications

--- frontend/src/componenet/Categories/Categories.js
+++ frontend/src/componenet/Categories/Categories.js
@@ -1,6 +1,16 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { Routes, Route, Link } from "react-router-dom";
+import axios from "axios";
+import jwt_decode from "jwt-decode";
+import { useNavigate } from "react-router-dom";
+import { toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import { useDispatch, useSelector } from "react-redux";
+import { login, logout } from "../reducer/login/index";
+import moment from "moment";
 import "./Categories.css";
 const Categories = () => {
+
   return (
     <div className="categories">
       <section className="category-section">
