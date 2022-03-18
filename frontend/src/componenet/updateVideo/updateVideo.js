a21027ad4019dc982e13a19f4544eb84b1f7dd8e
NasserQasim <nasseralotaiby1@gmail.com>
2022-03-18 14:48:01 +0300

    video Cloudinary

--- frontend/src/componenet/updateVideo/updateVideo.js
+++ frontend/src/componenet/updateVideo/updateVideo.js
@@ -281,7 +281,7 @@ const UpdateAnVideoById = () => {
         <div className="product-info">
           <p className="text">Video Link</p>
 
-          <Cloudinary setImage={setvideo_link} />
+          <CloudinaryVideo setVideo={setvideo_link} />
         </div>
 
         <div className="buttons">
