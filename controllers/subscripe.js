f11c7a97eaa298ac4f9d0e6bbfb0f3d1c74cb125
MohamadAbdulraheem <mohammad.o.abdulraheem@gmail.com>
2022-03-19 11:11:40 +0300

    structure change

---
+++ controllers/subscripe.js
@@ -0,0 +1,116 @@
+const { connection } = require("../database/db");
+
+const addToSubscription = (req, res) => {
+  const chanelId = req.params.id;
+  const userId = req.token.userId;
+  const query = `insert into subscriptions (chanel_id , user_id) values (?,?)`;
+
+  const data = [chanelId, userId];
+  connection.query(query, data, (err, result) => {
+    if (err) {
+      return res.status(500).json({
+        success: false,
+        message: `Something went wrong in subscription`,
+        err: err,
+      });
+    }
+    res.status(200).json({
+      success: true,
+      message: `supscription done successfully`,
+      results: result,
+    });
+  });
+};
+
+const removeFromMySubscription = (req, res) => {
+  const chanelId = req.params.id;
+  const userId = req.token.userId;
+  const query = `update subscriptions set is_deleted = 1 where user_id = ? and chanel_id = ? where is_deleted=0`;
+
+  const data = [chanelId, userId];
+  connection.query(query, data, (err, result) => {
+    if (err) {
+      return res.status(500).json({
+        success: false,
+        message: `Something went wrong in subscription`,
+        err: err,
+      });
+    }
+    res.status(200).json({
+      success: true,
+      message: `supscription done successfully`,
+      results: result,
+    });
+  });
+  ``;
+};
+
+const getMySubscriptionChannels = (req, res) => {
+  const userId = req.token.userId;
+  const query = `select users.id,video_views,firstName,videos.publish_date,description,videos.id, lastName,users.user_image ,video_link,videos.image,title from subscriptions inner join users on subscriptions.chanel_id = users.id inner join videos on videos.user_id = users.id where subscriptions.user_id=? `;
+  const data = [userId];
+  connection.query(query, data, (err, result) => {
+    if (err) {
+      console.log(err);
+      res
+        .status(404)
+        .json({ success: false, message: "server error", err: err });
+    } else {
+      res
+        .status(200)
+        .json({ success: true, message: `All the channels`, results: result });
+    }
+  });
+};
+
+
+
+const getsubscribersofchanel = (req, res) => {
+  const userId = req.params.id;
+  const query = `SELECT * from subscriptions WHERE chanel_id = ? `;
+  const data = [userId];
+  connection.query(query, data, (err, result) => {
+    if (err) {
+      console.log(err);
+      res
+        .status(404)
+        .json({ success: false, message: "server error", err: err });
+    } else {
+      res
+        .status(200)
+        .json({ success: true, message: `All the channels`, results: result });
+    }
+  });
+};
+
+
+const getAllvideossubscripes = (req, res) => {
+  const userId = req.token.userId;
+  const query = `select users.id as user_id,video_views,firstName,videos.publish_date,description,videos.id, lastName,users.user_image ,video_link,videos.image,title from subscriptions inner join users on subscriptions.chanel_id = users.id inner join videos on videos.user_id = users.id where subscriptions.user_id=? `;
+  const data = [userId];
+  connection.query(query, data, (err, result) => {
+    if (err) {
+      console.log(err);
+      res
+        .status(404)
+        .json({ success: false, message: "server error", err: err });
+    } else {
+      if (!result.length) {
+        return res
+          .status(200)
+          .json({ success: false, message: "No videos", err: err });
+      }
+      res
+        .status(200)
+        .json({ success: true, message: `All the Videos`, results: result });
+    }
+  });
+};
+
+module.exports = {
+  addToSubscription,
+  removeFromMySubscription,
+  getMySubscriptionChannels,
+  getAllvideossubscripes,
+  getsubscribersofchanel
+};
---
+++ index.js
@@ -0,0 +1,46 @@
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+const app = express();
+const db = require("./database/db");
+const { videoRouter } = require("./routes/videoRouter");
+const { userRouter } = require("./routes/userRouter");
+const { roleRouter } = require("./routes/roleRouter");
+const { commentRouter } = require("./routes/commentRouter");
+const { watch_laterRouter } = require("./routes/watchLaterRouter");
+const { LikedRouter } = require("./routes/likeRouter");
+const { subsciptionRouter } = require("./routes/subscriptionRouter");
+app.use(cors());
+
+app.use(express.json());
+
+//create video route with path of "/video"
+app.use("/video", videoRouter);
+
+//create user route with path of "/user"
+
+app.use("/user", userRouter);
+
+//create comment route with path of "/comment"
+
+app.use("/comment", commentRouter);
+
+//create role route with path of "/role"
+
+// app.use("/role", roleRouter);
+
+//create watchLater route with path of "/watchLater"
+
+app.use("/watchLater", watch_laterRouter);
+
+//create like route with path of "/like"
+
+app.use("/like", LikedRouter);
+
+app.use("/subscription", subsciptionRouter);
+
+const PORT = 5000;
+
+app.listen(PORT, () => {
+  console.log(`SERVER WORKING ON PORT: ${PORT}`);
+});
