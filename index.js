1003caf8690d2e4bc066466b833f0348de357005
MohamadAbdulraheem <mohammad.o.abdulraheem@gmail.com>
2022-03-19 11:26:54 +0300

    deploy

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const cors = require("cors");
+const path = require("path");
+
 require("dotenv").config();
 const app = express();
 const db = require("./database/db");
@@ -13,6 +15,13 @@ const { subsciptionRouter } = require("./routes/subscriptionRouter");
 app.use(cors());
 
 app.use(express.json());
+
+if(process.env.NODE_ENV === 'production'){
+  app.use(express.static('client/build'));
+}
+app.get('*',(req, res) => {
+  res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+});
 
 //create video route with path of "/video"
 app.use("/video", videoRouter);
